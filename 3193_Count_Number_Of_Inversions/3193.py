c4ed9506889553f434638430f15079abbd215e68
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2024-12-23 20:20:15 -0300

    tentativa inicial

--- 3193_Count_Number_Of_Inversions/3193.py
+++ 3193_Count_Number_Of_Inversions/3193.py
@@ -1,8 +1,14 @@
-class Solution(object):
-    def numberOfPermutations(self, n, requirements):
-        """
-        :type n: int
-        :type requirements: List[List[int]]
-        :rtype: int
-        """
-        
+class Solution:
+    def solve(self, indice, tamanho, mapa_requisitos, contador):
+        if indice == tamanho:
+            return 1
+
+        total_permutacoes = 0
+
+        for numero in range(contador, contador + indice + 1):
+            total_permutacoes += self.solve(indice + 1, tamanho, mapa_requisitos, numero)
+
+        return total_permutacoes
+
+    def numberOfPermutations(self, tamanho, requisitos):
+        return self.solve(1, tamanho, {}, 0)
