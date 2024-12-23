2d1d03aea88d51ab258c0645432029b470e4fff0
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2024-12-23 20:48:43 -0300

    Nova versão

--- 3193_Count_Number_Of_Inversions/3193.py
+++ 3193_Count_Number_Of_Inversions/3193.py
@@ -1,14 +1,26 @@
 class Solution:
+    def __init__(self):
+        self.dp = {}
+        self.mod = int(1e9 + 7)
+
     def solve(self, indice, tamanho, mapa_requisitos, contador):
         if indice == tamanho:
             return 1
 
+        if (indice, contador) in self.dp:
+            return self.dp[(indice, contador)]
+
         total_permutacoes = 0
 
         for numero in range(contador, contador + indice + 1):
-            total_permutacoes += self.solve(indice + 1, tamanho, mapa_requisitos, numero)
+            total_permutacoes = (total_permutacoes + self.solve(indice + 1, tamanho, mapa_requisitos, numero)) % self.mod
 
+        self.dp[(indice, contador)] = total_permutacoes
         return total_permutacoes
 
     def numberOfPermutations(self, tamanho, requisitos):
-        return self.solve(1, tamanho, {}, 0)
+        mapa_requisitos = {}
+        for requisito in requisitos:
+            mapa_requisitos[requisito[0]] = requisito[1]
+
+        return self.solve(1, tamanho, mapa_requisitos, 0)
