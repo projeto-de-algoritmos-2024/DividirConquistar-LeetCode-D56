7e1549b71b890c13a84193c9bcfce906488665d1
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2024-12-23 21:12:55 -0300

    3193 - Nova Versão - Accepted

--- 3193_Count_Number_Of_Inversions/3193.py
+++ 3193_Count_Number_Of_Inversions/3193.py
@@ -7,12 +7,18 @@ class Solution:
         if indice == tamanho:
             return 1
 
+        if contador > 400:
+            return 0
+
         if (indice, contador) in self.dp:
             return self.dp[(indice, contador)]
 
         total_permutacoes = 0
 
         for numero in range(contador, contador + indice + 1):
+            if indice in mapa_requisitos and mapa_requisitos[indice] != numero:
+                continue
+
             total_permutacoes = (total_permutacoes + self.solve(indice + 1, tamanho, mapa_requisitos, numero)) % self.mod
 
         self.dp[(indice, contador)] = total_permutacoes
@@ -22,5 +28,8 @@ class Solution:
         mapa_requisitos = {}
         for requisito in requisitos:
             mapa_requisitos[requisito[0]] = requisito[1]
+
+        if 0 in mapa_requisitos and mapa_requisitos[0] != 0:
+            return 0
 
         return self.solve(1, tamanho, mapa_requisitos, 0)
